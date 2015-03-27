ceee5c4f8cbdbc247377067cce1dcca4c9be591a
csrgxtu <1246506786@qq.com>
2015-03-27 09:27:25 +0800

    add log4js test

---
+++ test/log4jsexample.js
@@ -0,0 +1,13 @@
+var log4js = require('log4js');
+log4js.loadAppender('file');
+log4js.addAppender(log4js.appenders.file('../logs/cheese.log'), 'cheese');
+
+var logger = log4js.getLogger('cheese');
+logger.setLevel('ERROR');
+
+logger.trace('Entering cheese testing');
+logger.debug('Got cheese.');
+logger.info('Cheese is Gouda.');
+logger.warn('Cheese is quite smelly.');
+logger.error('Cheese is too ripe!');
+logger.fatal('Cheese was breeding ground for listeria.');
