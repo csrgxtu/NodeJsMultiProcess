90ef01bd34483ad0dab8246f3e44697a4a0cdafc
csrgxtu <1246506786@qq.com>
2015-03-27 09:09:53 +0800

    fork example

---
+++ docs/resource.md
@@ -0,0 +1,2 @@
+# Resources
+[Who is Ryan Dahl?](http://www.quora.com/Who-is-Ryan-Dahl)
---
+++ src/worker.js
@@ -0,0 +1,5 @@
+var http = require('http')
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.send('Hello World\n');
+}).listen(Math.round((1 + Math.random()) * 1000), '127.0.0.1');
